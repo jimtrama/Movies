be8d61fc3bf0a9f9d40531b98603d69b03255ea3
Dimitrios <dimeragon@gmail.com>
2024-09-08 19:24:48 +0300

    account upadated

--- src/app/Pages/home/account-page/account-content.component.ts
+++ src/app/Pages/home/account-page/account-content.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AccountService } from '../../../Services/account.service';
 import Account from '../../../Models/account.model';
 
+
+type AccountRes = "email" | "wallet" | "first_name" | "last_name";
 @Component({
   selector: 'app-account-content',
   templateUrl: './account-content.component.html',
@@ -10,6 +12,30 @@ import Account from '../../../Models/account.model';
 export class AccountContentComponent {
 
   account:Account = new Account();
+
+  accountPros:{key:AccountRes,title:string,icon:string}[] = [
+    {
+      title:"Email",
+      key:"email",
+      icon:"money.png"
+    },
+    {
+      title:"Email",
+      key:"first_name",
+      icon:"money.png"
+    },
+    {
+      title:"Email",
+      key:"last_name",
+      icon:"money.png"
+    },
+    {
+      title:"Email",
+      key:"wallet",
+      icon:"money.png"
+    },
+
+  ];
 
   constructor(private accountService:AccountService){
     this.accountService.account.subscribe(a=>{
